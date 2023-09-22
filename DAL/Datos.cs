5f217149313581a4c5958f604d53f56b39eb0277
isatkm21 <isabellamanjarres67@gmail.com>
2023-09-22 15:16:30 -0500

    cuarto cambio

--- DAL/Datos.cs
+++ DAL/Datos.cs
@@ -4,9 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL
+namespace Datos
+
 {
     public class Class1
     {
+        public class Liquidacioncuotamoderadora
+        {
+            public void insertarLiquidacion(Liquidacioncuotamoderadora liquidacion)
+            {
+                using (var context = new MyDbContext())
+                {
+                    context.Liquidacioncuotamoderadora.add(liquidacion);
+                    context.SaveChanges();
+                }
+            }
+            public Liquidacioncuotamoderadora obtenerliquidacionpornumero(int numeroliquidacion)
+            {
+                using(var context = new MyDbContext())
+                {
+                    return context.Liquidacioncuotamoderadora.FirsOrDefault(1=> 1.Numero liquidacion == numeroliquidacion);
+
+                }
+
+            }
+            public void Actualizarliquidacion(Liquidacioncuotamoderadora liquidacion)
+            {
+                using (var context = new MyDbcontext())
+                {
+                    // Marca la liquidación como modificada para que Entidades Framework realice una actualización
+                    context.Entry(liquidacion).State = EntidadesState.Modified;
+                    context.SaveChanges();
+                }
+            }
+                
+
+
+        }
+        
     }
 }
